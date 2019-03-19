f47ba43830e6cc5d587e0a62f4c7848b09711e31
Guillaume Grossetie <ggrossetie@gmail.com>
2019-03-19 19:49:08 +0100

    Fix bug, catch error and exit

--- tasks/publish.js
+++ tasks/publish.js
@@ -7,18 +7,25 @@ const { publish } = require('libnpmpublish')
 const projectRootDirectory = path.join(__dirname, '..')
 
 ;(async () => {
-  // publish core package
-  const corePkgName = childProcess.execSync(`npm pack`, { cwd: path.join(projectRootDirectory, 'packages', 'core') }).toString('utf-8').trim()
-  const corePkg = require('../packages/core/package.json')
-  const coreTarball = fs.createReadStream(path.join(projectRootDirectory, 'packages', 'core', corePkgName))
-  await publish(corePkg, coreTarball, {
-    token: process.env.NPM_AUTH_TOKEN
-  })
-  // publish main package
-  const mainPkgName = childProcess.execSync(`npm pack`, { cwd: path.join(projectRootDirectory, 'packages', 'main') }).toString('utf-8').trim()
-  const mainPkg = require('../packages/main/package.json')
-  const mainTarball = fs.createReadStream(path.join(projectRootDirectory, mainPkgName))
-  await publish(mainPkg, mainTarball, {
-    token: process.env.NPM_AUTH_TOKEN
-  })
+  try {
+    // publish core package
+    const coreDirectory = path.join(projectRootDirectory, 'packages', 'core')
+    const corePkgName = childProcess.execSync(`npm pack`, { cwd: coreDirectory }).toString('utf-8').trim()
+    const corePkg = require('../packages/core/package.json')
+    const coreTarball = fs.createReadStream(path.join(coreDirectory, corePkgName))
+    await publish(corePkg, coreTarball, {
+      token: process.env.NPM_AUTH_TOKEN
+    })
+    // publish main package
+    const mainDirectory = path.join(projectRootDirectory, 'packages', 'main')
+    const mainPkgName = childProcess.execSync(`npm pack`, { cwd: mainDirectory }).toString('utf-8').trim()
+    const mainPkg = require('../packages/main/package.json')
+    const mainTarball = fs.createReadStream(path.join(mainDirectory, mainPkgName))
+    await publish(mainPkg, mainTarball, {
+      token: process.env.NPM_AUTH_TOKEN
+    })
+  } catch (e) {
+    console.log('Unable to publish the packages', e)
+    process.exit(1)
+  }
 })()
